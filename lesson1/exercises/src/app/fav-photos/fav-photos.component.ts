aa1ebbf3066b4b703ed0478864fefb91e678bfd5
Fe1ky <76542408+Fe1ky@users.noreply.github.com>
2021-08-08 12:30:50 -0500

    completed Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://pbs.twimg.com/media/E8NGQfKWEAEiOMm.png';
+  image3 = 'https://2.bp.blogspot.com/--00gZMQbdvU/UyJ0PFNsVuI/AAAAAAAAAeI/P0ms31K-p1Q/s1600/Peducar+color+back.JPG';
 
   constructor() { }
 
