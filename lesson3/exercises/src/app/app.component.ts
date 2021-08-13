ae619c9306c83ca4044b9f58b897bd326d834c77
Fe1ky <76542408+Fe1ky@users.noreply.github.com>
2021-08-12 20:23:45 -0500

    Exercise 20 completed!

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,84 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
+
+
+  gutterCheck(width,height){
+    if(width > 26000 || width < 0 || height < 0 || height>34000){
+      this.color ='orange'
+      
+    }else {
+      this.color= 'blue'
+    }
+    
+  }
+
+
+  moveRocket(rocketImage, direction){
+    if(direction === 'right'){
+      const movement = parseInt(rocketImage.style.left)+10+'px';
+      rocketImage.style.left= movement;
+      this.width= this.width +10000
+    }
+    if(direction === 'left'){
+      const movement = parseInt(rocketImage.style.left)-10+'px';
+      rocketImage.style.left= movement;
+      this.width= this.width -10000
+    }
+    if(direction === 'up'){
+      const movement = parseInt(rocketImage.style.bottom)+10+'px';
+      rocketImage.style.bottom = movement;
+      this.width= this.width +10000
+    }
+
+    if(direction === 'down'){
+      const movement = parseInt(rocketImage.style.bottom)-10+'px';
+      rocketImage.style.bottom= movement;
+      this.width= this.width -10000
+    }
+
+    this.gutterCheck(this.width, this.height);
+    
+  }
+
+  handleTakeOff(rocketImage) {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+       this.takeOffEnabled = false;
+    }
+ }
+
+ handleLand(rocketImage) {
+  let result = window.confirm('Are you sure you wantt to return to earth?');
+  if (result) {
+     this.color = 'green';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Shuttle has landed.';
+     rocketImage.style.bottom = '0px'
+     this.takeOffEnabled = true;
+  }
+}
+
+handleMissionAbort(rocketImage) {
+  let result = window.confirm('Are you sure you want to abort the mission');
+  if (result) {
+     this.color = 'red';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Mission Aborted';
+     rocketImage.style.bottom = '0px'
+     this.takeOffEnabled = true;
+  }
+}
+
+
+
+
+
 }
